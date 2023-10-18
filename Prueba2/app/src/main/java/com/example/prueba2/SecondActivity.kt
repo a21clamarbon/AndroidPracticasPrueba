93645df067c9d27247eb22a61794c53c21b9a66e
MrM250 <marcamor250@gmail.com>
2023-10-18 18:31:21 +0200

    Local-Cole

---
+++ Prueba2/app/src/main/java/com/example/prueba2/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.example.prueba2
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Verd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.decorView.setBackgroundColor(Color.GREEN)
+    }
+}
+
+class Blau : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.decorView.setBackgroundColor(Color.BLUE)
+    }
+}
+
+class Vermell : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.decorView.setBackgroundColor(Color.RED)
+    }
+}
